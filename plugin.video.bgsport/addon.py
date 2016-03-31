c9731e8246e284f25ecf8b65b80972de4f3b10b6
Harry G <harrygg@gmail.com>
2016-03-31 20:41:59 +0300

    working cats

--- plugin.video.bgsport/addon.py
+++ plugin.video.bgsport/addon.py
@@ -2,7 +2,7 @@
 import sys, os
 from xbmcswift2 import Plugin
 import urlresolver
-#from resources.lib.classes import *
+from resources.lib.classes import *
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -11,26 +11,60 @@ plugin = Plugin('plugin.video.bgsport')
 #plugin entry screen camera categories
 @plugin.route('/')
 def index():
-	sources = [urlresolver.HostedMediaFile(url='http://www.dailymotion.com/video/x3z3l2c', title='dailymotion')]
-	plugin.log.info("sources len: " + str(len(sources)))
+	#sources = [urlresolver.HostedMediaFile(url='http://www.dailymotion.com/video/x3z3l2c', title='dailymotion')]
+	#plugin.log.info("sources len: " + str(len(sources)))
 	#source = urlresolver.choose_source(sources)
 	#if source:
 	#	stream_url = source.resolve()
 	#	addon.resolve_url(stream_url)
 	#else:
 	#	addon.resolve_url(False)
-	media_url = urlresolver.resolve('http://btvplus.bg/produkt/predavaniya/1810')
-	plugin.log.info("media_url: " + media_url)
-	stream_url = urlresolver.HostedMediaFile(host='dailymotion.com', media_id='x3z3l2c').resolve()
-	plugin.log.info("stream_url: " + stream_url)
-	
+	#media_url = urlresolver.resolve('http://btvplus.bg/produkt/predavaniya/1810')
+	#plugin.log.info("media_url: " + media_url)
+	#stream_url = urlresolver.HostedMediaFile(host='dailymotion.com', media_id='x3z3l2c').resolve()
+	#plugin.log.info("stream_url: " + stream_url)
+	cats = get_categories()
 	items = [{
-		'label': "Dailymotion", 
-		'path': media_url, 
-		'is_playable': True
-	}]
+		'label': cat.name, 
+		'path': plugin.url_for('show_category', category_id=cat.id), 
+		'is_playable': False
+	} for cat in cats]
 	return items
 
+def get_categories(category_id = 0):
+	conn = sqlite3.connect(helper.local_db)
+	cursor = conn.execute('''SELECT * FROM categories WHERE parent_id == 0''',  [category_id])
+	categories = [Category(row) for row in cursor] 
+	return categories
+
+@plugin.route('/categories/<category_id>/')
+def show_category(category_id):
+	items = []
+	cats = get_categories(category_id)
+	
+	items.append({
+			'label' : cat.name, 
+			'path' : plugin.url_for('show_videos', category_id=cat.id),
+			'icon' : '',
+			'is_playable' : False
+		})
+	return items
+	#return plugin.finish(items, view_mode=500)
+
+@plugin.route('/videos/<category_id>/')
+def show_videos(category_id):
+	items = []
+	cats = get_videos(category_id)
+		
+	items.append({
+			'label' : video.name, 
+			'path' : plugin.url_for('play_videos', video_id=cat.id),
+			'icon' : '',
+			'is_playable' : False
+		})
+	return items
+	#return plugin.finish(items, view_mode=500)
+	
 #Play camera stream
 #@plugin.route('/play/<url>/')
 #def play_stream(camera_id):
--- plugin.video.bgsport/resources/lib/classes.py
+++ plugin.video.bgsport/resources/lib/classes.py
@@ -11,112 +11,16 @@ class Category:
 	def __init__(self, attr):
 		self.id = attr[0]
 		self.name = attr[1]
-		self.count = attr[2]
-
-
-### Class Camera
-class Camera:
-
-	def __init__(self, attr):
-		l = len(attr)
-		self.id = attr[0]
-		self.inactive = attr[2]
-		self.name = attr[3]
-		self.stream = '' if attr[4] == None else attr[4]
-		if l > 5:
-			self.stream_rtsp = '' if attr[5] == None else attr[5]
-			if l > 6:
-				self.player_url = attr[6]
-				if l > 7:
-					self.page_url = attr[7]
-					if l > 8:
-						self.logo = attr[8]
-
-	def _resolve(self):
-		from urlparse import urlparse
-		stream = ''
-		headers = {
-			'User-Agent' : 'stagefright',
-			'Referer' : self.page_url }
-
-		r = requests.get(self.player_url, headers=headers)
-		parser = urlparse(self.player_url)
-		if parser.hostname == 'ipcamlive.com':
-			m = re.compile('address[=\s\'"]+(.*?)[\'"\s]+', re.DOTALL).findall(r.text)
-			if len(m) > 0:
-				stream = m[0]
-			m = re.compile('streamid[=\s\'"]+(.*?)[\'"\s]+', re.DOTALL).findall(r.text)
-			if len(m) > 0:
-				stream = '%s/streams/%s/stream.m3u8' % (stream, m[0])
-		else: #regular video stream
-			m = re.compile('video.+src[=\s"\']+(.+?)[\s"\']+', re.DOTALL).findall(r.text)
-			if len(m) > 0:
-				stream = m[0]
-		return stream
-
-	def get_stream(self):
-		if self.stream != '' and self.stream != None:
-			return self.stream
-		elif self.stream_rtsp != '' and self.stream_rtsp != None:
-			return self.stream_rtsp
-		elif self.player_url != '' and self.player_url != None:
-			return self._resolve()
-		else: 
-			return ''
-
-	def get_icon(self):
-		try:
-			import time
-			ts = int(time.time())
-			if '?' in self.logo:
-				if self.logo.endswith('='):
-					return "%s%s" % (self.logo, ts)
-				else:
-					return "%s&amp;ts=%s" % (self.logo, ts)
-			else:
-				return "%s?ts=%s" % (self.logo, ts)
-		except:
-			return ''
-
-
-### Class PrivateCameras
-class PrivateCameras:
-
-	def __init__(self, plugin):
-		self.plugin = plugin
-		self.id = 0
-		self.name = 'Частни камери'
-		self.cameras = []
-		self.count = 0
-
-		if plugin.addon.getSetting('usePrivateList') == "1":
-			plf = plugin.addon.getSetting('privateListFile')
-			if os.path.exists(plf):
-				with open(plf) as f:
-					try:
-						c = f.read()
-						names = re.compile('#EXTINF:\-1,\s*(.*)').findall(c)
-						urls = re.compile('(http.*|rtsp.*|rtmp.*)').findall(c)
-						if len(names) == len(urls):
-							for i in range(0, len(names)):
-								cam = Camera(['p'+str(i), 0, 0, names[i], urls[i]])
-								self.cameras.append(cam)
-								self.count = self.count + 1
-					except Exception, er:
-						self.plugin.log.error(str(er))
-
-	def get_camera(self, id = 'p'):
-		for c in self.cameras:
-			if c.id == id:
-				return Camera([c.id, 0, 0, c.name, c.stream])
-
+		self.parent_id = attr[2]
 
 ### Class Helper
 class Helper:
+	
+	db_file_name = 'assets.db'
 
 	def __init__(self, plugin):
 		self.plugin = plugin
-		self.local_db = os.path.join(plugin.storage_path, 'assets.sqlite')
+		self.local_db = os.path.join(plugin.storage_path, self.db_file_name)
 
 	def check_assets(self):
 		#Check whether the assets file is old
@@ -131,14 +35,13 @@ class Helper:
 					self.download_assets()
 		except Exception, er:
 			self.plugin.log.error(er)
-			xbmc.executebuiltin('Notification(%s,%s,10000,%s)' % ('БГ Камерите','Неуспешно сваляне на най-новия списък с камери',''))
-			assets = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../storage/assets.sqlite.gz')
+			xbmc.executebuiltin('Notification(%s,%s,10000,%s)' % (self.plugin.name,'Неуспешно сваляне на най-новата база данни',''))
+			assets = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../storage/%s.gz' % self.db_file_name)
 			self.extract(assets)
 	
 	def download_assets(self):
 		try:
-			id = 'plugin.video.bgcameras'
-			remote_db = 'http://rawgit.com/harrygg/%s/sqlite/%s/resources/storage/assets.sqlite.gz?raw=true' % (id, id)
+			remote_db = 'http://github.com/harrygg/bgsport/blob/master/%s/resources/storage/%s.gz?raw=true' % (self.plugin.id, self.plugin.db_file_name)
 			self.plugin.log.info('Downloading assets from url: %s' % remote_db)
 			save_to_file = self.local_db if '.gz' not in remote_db else self.local_db + ".gz"
 			f = urllib2.urlopen(remote_db)
